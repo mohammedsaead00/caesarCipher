25af2826be81e44340d22b1b79ffd8d348a4dc3f
Mohammed Saeed <mohammedsaead643@gmail.com>
2025-08-09 00:18:02 +0300

    edits

---
+++ caesarCipher/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.IO.Pipelines;
+namespace caesarCipher;
+
+  class Program
+{
+    static void Main(string[] args)
+    {
+        char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        Console.WriteLine("Enter a message");
+        string result = Console.ReadLine();
+        result = result.ToLower();
+        char[] secretMessage = result.ToCharArray();
+        char[] encryptedMessage = new char[secretMessage.Length];
+        for (int i = 0; i < secretMessage.Length; i++)
+        {
+            char ch = secretMessage[i];
+            int position = Array.IndexOf(alphabet, ch);
+            int newLetterPosition = (position + 3) % alphabet.Length;
+            char encryptedLetter = alphabet[newLetterPosition];
+            encryptedMessage[i] = encryptedLetter;
+        }
+        Console.WriteLine(String.Join("", encryptedMessage));
+    }
+}
